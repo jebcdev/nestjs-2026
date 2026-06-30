31793ef14abcfc3964f9ba44df3da9a15dc858af
esteban5tael@gmail.com <esteban5tael@gmail.com>
2026-06-29 21:00:25 -0500

    creacion de el recurso completo de pokemons

--- 02-pokedex/src/app.module.ts
+++ 02-pokedex/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PokemonsModule } from './pokemons/pokemons.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // Adjust the path to your static files
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }), // const envVar = this.configService.get<string>('envVar');
+    TypeOrmModule.forRoot({
+      type: (process.env.DB_TYPE as any) || 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'nestjs-pokedex',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }), PokemonsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- 02-pokedex/src/main.ts
+++ 02-pokedex/src/main.ts
@@ -1,8 +1,49 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger, ValidationPipe } from "@nestjs/common";
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  /*  */
+
+  const logger: Logger = new Logger(
+    "----- Bootstrap -----"
+);
+
+const app = await NestFactory.create(AppModule);
+
+// Obtener ConfigService del contenedor de dependencias
+const configService = app.get(ConfigService);
+
+const PORT: number = configService.get<number>('PORT') || 3000;
+const API_PREFIX: string = configService.get<string>('API_PREFIX') || "api/v1";
+
+app.setGlobalPrefix(API_PREFIX);
+app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true, // Automatically transform payloads to DTO instances
+        transformOptions: {
+            enableImplicitConversion: true, // Allow implicit conversion of types
+        },
+    })
+);
+
+// Configuración de CORS
+app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+});
+
+await app.listen(PORT);
+logger.log(
+    `Application is running on: http://localhost:${PORT}/${API_PREFIX}`
+);
+logger.log(
+    `Environment: ${configService.get<string>('NODE_ENV') || 'development'}`
+);
 }
 bootstrap();
